0840fde5da6f7a2f0d8939b2adb40a222e153764
Tanmay Gupta <tanmay2099@gmail.com>
2018-03-24 11:52:33 -0500

    runner for select_confident_boxes

--- exp/detect_coco_objects/run.py
+++ exp/detect_coco_objects/run.py
@@ -1,11 +1,15 @@
 import exp.detect_coco_objects.prepare_data_for_faster_rcnn as \
     prepare_data_for_faster_rcnn
+import exp.detect_coco_objects.select_confident_boxes as \
+    select_confident_boxes
+import exp.detect_coco_objects.evaluate_boxes as evaluate_boxes
 from data.hico.hico_constants import HicoBoxesConstants
 from utils.constants import ExpConstants
 from exp.experimenter import *
 
+
 def exp_detect_coco_objects():
-    exp_name = 'detect_coco_objects'
+    exp_name = 'detect_coco_objects_in_hico'
     exp_const = ExpConstants(
         exp_name=exp_name,
         out_base_dir='/home/tanmay/Data/weakly_supervised_hoi_exp')
@@ -16,6 +20,32 @@ def exp_detect_coco_objects():
 
     prepare_data_for_faster_rcnn.prepare(exp_const,data_const)
 
+
+def exp_select_confident_boxes():
+    exp_name = 'select_confident_boxes'
+    exp_const = ExpConstants(
+        exp_name=exp_name,
+        out_base_dir='/home/tanmay/Data/weakly_supervised_hoi_exp')
+    exp_const.background_score_thresh = 0.01
+    exp_const.max_humans = 10
+    exp_const.max_objects_per_class = 10
+    exp_const.max_background = 1
+    exp_const.iou_thresh = 0.5
+
+    data_const = HicoBoxesConstants(
+        clean_dir='/home/ssd/hico_det_clean_20160224',
+        proc_dir='/home/ssd/hico_det_processed_20160224')
+
+    human_score_thresholds = [0.01,0.05,0.1,0.5]
+    object_score_thresholds = [0.01,0.05,0.1,0.5]
+    
+    for human_score_thresh in human_score_thresholds:
+        for object_score_thresh in object_score_thresholds:
+            exp_const.human_score_thresh = human_score_thresh
+            exp_const.object_score_thresh = object_score_thresh
+            
+            select_confident_boxes.select(exp_const,data_const)
+            evaluate_boxes.evaluate(exp_const,data_const)
 
 if __name__=='__main__':
     list_exps(globals())
