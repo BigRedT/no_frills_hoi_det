71dbfcda35066591f6f8e67c5be7f456d788abf2
tgupta6 <tgupta6@illinois.edu>
2018-04-02 11:48:33 -0500

    add vis_bbox, vis_bboxes, join_bboxes_by_line, vis_sub_obj_bboxes in bbox_utils.py

--- utils/bbox_utils.py
+++ utils/bbox_utils.py
@@ -46,3 +46,72 @@ def compute_iou(bbox1,bbox2):
 
     return iou 
     
+
+def vis_bbox(bbox,img,color=(0,0,0),modify=False):
+    x1,y1,x2,y2 = bbox
+    r = [y1,y1,y2,y2]
+    c = [x1,x2,x2,x1]
+    rr,cc = skdraw.polygon_perimeter(r,c,img.shape[:2])
+    #rr,cc = skdraw.polygon(r,c,img.shape[:2])
+
+    if modify:
+        img_ = img
+    else:
+        img_ = np.copy(img)
+
+    #skdraw.set_color(img_,(rr,cc),color,alpha=0.1)
+    for k in range(3):
+        img_[rr,cc,k] = color[k]
+
+    return img_
+
+
+def vis_bboxes(bboxes,img,color=(0,0,0),modify=False):
+    if modify:
+        img_ = img
+    else:
+        img_ = np.copy(img)
+
+    for bbox in bboxes:
+        img_ = vis_bbox(bbox,img_,color,True)
+
+    return img_
+
+
+def join_bboxes_by_line(bbox1,bbox2,img,color=(0,0,255),modify=False):
+    x1,y1,x2,y2 = bbox1
+    x1_,y1_,x2_,y2_ = bbox2
+
+    c0 = 0.5*(x1+x2)
+    r0 = 0.5*(y1+y2)
+    c1 = 0.5*(x1_+x2_)
+    r1 = 0.5*(y1_+y2_)
+    r0,c0,r1,c1 = [int(x) for x in [r0,c0,r1,c1]]
+    rr,cc,val = skdraw.draw.line_aa(r0,c0,r1,c1)
+    
+    if modify:
+        img_ = img
+    else:
+        img_ = np.copy(img)
+
+    for k in range(3):
+        img_[rr,cc,k] = val*color[k]
+
+    return img_
+
+
+def vis_sub_obj_bboxes(
+        sub_bboxes,
+        obj_bboxes,
+        img,
+        sub_color=(0,0,255),
+        obj_color=(0,255,0),
+        modify=False):
+
+    img_ = vis_bboxes(sub_bboxes,img,sub_color,modify)
+    img_ = vis_bboxes(obj_bboxes,img_,obj_color,modify=True)
+    
+    for sub_bbox,obj_bbox in zip(sub_bboxes,obj_bboxes):
+        img_ = join_bboxes_by_line(sub_bbox,obj_bbox,img_,modify=True)
+
+    return img_
