85c83e2571cb22b6dfd4d66dbae70eb5ef6affee
tgupta6 <tgupta6@illinois.edu>
2018-04-02 11:49:24 -0500

    add save_constants method in utils/constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -16,3 +16,10 @@ class ExpConstants(Constants):
         self.exp_name = exp_name
         self.out_base_dir = out_base_dir
         self.exp_dir = os.path.join(self.out_base_dir,self.exp_name)
+
+
+def save_constants(name_const_dict,outdir):
+    for name, const in name_const_dict.items():
+        print(f'Saving {name} constants ...')
+        filename = os.path.join(outdir,f'{name}_constants.json')
+        const.to_json(filename)
