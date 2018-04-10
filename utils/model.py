3aa31ade0805e1f733130a509c5fe1024c0bf0e7
Tanmay Gupta <tanmay2099@gmail.com>
2018-04-10 11:06:16 -0500

    add Model class in utils/model.py

---
+++ utils/model.py
@@ -0,0 +1,32 @@
+import os
+
+import utils.io as io
+
+
+class Model(io.WritableToFile):
+    def __init__(self):
+        super(Model,self).__init__()
+
+    def to_txt(self,dir_name,single_file=False):
+        if single_file:
+            print('Writing model.txt ...')
+            model_txt = os.path.join(dir_name,f'model.txt')
+            self.to_file(model_txt)
+            return
+
+        for model_name, model_instance in self.__dict__.items():
+            print(f'Writing {model_name}.txt ...')
+            model_txt = os.path.join(dir_name,f'{model_name}.txt')
+            model_instance.to_file(model_txt)
+
+    def __str__(self):
+        serialized = ''
+        for model_name, model_instance in self.__dict__.items():
+            serialized += '-'*80
+            serialized += '\n'
+            serialized += '\n'
+            serialized += model_instance.__str__()
+            serialized += '\n'
+            serialized += '\n'
+        serialized += '-'*80
+        return serialized
