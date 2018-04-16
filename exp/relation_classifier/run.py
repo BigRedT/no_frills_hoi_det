994d0760a1b0567f34c43bb825dc766ed92e7a14
Tanmay Gupta <tanmay2099@gmail.com>
2018-04-16 14:11:13 -0500

    add exp_train_balanced and exp_eval to relation_classifier/run.py

--- exp/relation_classifier/run.py
+++ exp/relation_classifier/run.py
@@ -10,7 +10,11 @@ from exp.relation_classifier.relation_classifier_model import \
 from exp.relation_classifier.gather_relation_model import \
     GatherRelationConstants
 import exp.relation_classifier.train as train
+import exp.relation_classifier.train_balanced as train_balanced
+import exp.relation_classifier.eval as evaluate
 from exp.relation_classifier.features import FeatureConstants
+from exp.relation_classifier.features_balanced import \
+    FeatureBalancedConstants
 
 parser.add_argument(
     '--gen_hoi_cand',
@@ -22,6 +26,28 @@ parser.add_argument(
     default=False,
     action='store_true',
     help='Apply this flag to label hoi candidates')
+parser.add_argument(
+    '--subset',
+    type=str,
+    default='train_val',
+    choices=['train','train_val','val','test'],
+    help='Apply this flag to specify subset of data')
+parser.add_argument(
+    '--imgs_per_batch',
+    type=int,
+    default=1,
+    help='Number of images per batch')
+parser.add_argument(
+    '--focal_loss',
+    default=False,
+    action='store_true',
+    help='Apply this flag to use focal loss instead of BCE')
+parser.add_argument(
+    '--fp_to_tp_ratio',
+    type=int,
+    default=4,
+    help='Number of images per batch')
+
 
 
 def exp_gen_and_label_hoi_cand():
@@ -30,7 +56,7 @@ def exp_gen_and_label_hoi_cand():
     exp_const = ExpConstants(
         exp_name=exp_name,
         out_base_dir='/home/tanmay/Data/weakly_supervised_hoi_exp')
-    exp_const.subset = 'test'
+    exp_const.subset = args.subset
 
     data_const = HicoConstants(
         clean_dir='/home/ssd/hico_det_clean_20160224',
@@ -51,7 +77,7 @@ def exp_gen_and_label_hoi_cand():
             f'hoi_candidates_{exp_const.subset}.hdf5')
         label_hoi_candidates.assign(exp_const,data_const)
 
-    
+
 def exp_train():
     exp_name = 'factors_rcnn_feats_scores'
     out_base_dir = \
@@ -60,14 +86,21 @@ def exp_train():
     exp_const = ExpConstants(
         exp_name=exp_name,
         out_base_dir=out_base_dir)
+    exp_const.log_dir = os.path.join(exp_const.exp_dir,'log')
+    exp_const.model_dir = os.path.join(exp_const.exp_dir,'models')
+    exp_const.num_epochs = 10
+    exp_const.batch_size = 1024
+    exp_const.lr = 1e-3
 
     data_const = FeatureConstants()
-    data_const.hoi_cands_hdf5 = '/home/tanmay/Data' + \
-        '/weakly_supervised_hoi_exp/hoi_candidates' + \
-        'hoi_candidates_train_val.hdf5'
-    data_const.hoi_cand_labels_hdf5 = '/home/tanmay/Data' + \
-        '/weakly_supervised_hoi_exp/hoi_candidates' + \
-        'hoi_candidate_labels_train_val.hdf5'
+    hoi_cand_dir = '/home/tanmay/Data' + \
+        '/weakly_supervised_hoi_exp/hoi_candidates'
+    data_const.hoi_cands_hdf5 = os.path.join(
+        hoi_cand_dir,
+        'hoi_candidates_train_val.hdf5')
+    data_const.hoi_cand_labels_hdf5 = os.path.join(
+        hoi_cand_dir,
+        'hoi_candidate_labels_train_val.hdf5')
     data_const.faster_rcnn_feats_hdf5 = os.path.join(
         data_const.proc_dir,
         'faster_rcnn_fc7.hdf5')
@@ -80,6 +113,86 @@ def exp_train():
     model_const.gather_relation.verb_list_json = data_const.verb_list_json
 
     train.main(exp_const,data_const,model_const)
+
+
+def exp_train_balanced():
+    args = parser.parse_args()
+    exp_name = 'factors_rcnn_feats_scores_' + \
+        f'imgs_per_batch_{args.imgs_per_batch}_' + \
+        f'focal_loss_{args.focal_loss}_' + \
+        f'fp_to_tp_ration_{args.fp_to_tp_ratio}'
+    out_base_dir = \
+        '/home/tanmay/Data/weakly_supervised_hoi_exp' + \
+        '/relation_classifier'
+    exp_const = ExpConstants(
+        exp_name=exp_name,
+        out_base_dir=out_base_dir)
+    exp_const.log_dir = os.path.join(exp_const.exp_dir,'log')
+    exp_const.model_dir = os.path.join(exp_const.exp_dir,'models')
+    exp_const.num_epochs = 10
+    exp_const.imgs_per_batch = args.imgs_per_batch
+    exp_const.lr = 1e-3
+    exp_const.focal_loss = args.focal_loss
+
+    data_const = FeatureBalancedConstants()
+    hoi_cand_dir = '/home/tanmay/Data' + \
+        '/weakly_supervised_hoi_exp/hoi_candidates'
+    data_const.hoi_cands_hdf5 = os.path.join(
+        hoi_cand_dir,
+        'hoi_candidates_train_val.hdf5')
+    data_const.hoi_cand_labels_hdf5 = os.path.join(
+        hoi_cand_dir,
+        'hoi_candidate_labels_train_val.hdf5')
+    data_const.faster_rcnn_feats_hdf5 = os.path.join(
+        data_const.proc_dir,
+        'faster_rcnn_fc7.hdf5')
+    data_const.fp_to_tp_ratio = args.fp_to_tp_ratio
+    data_const.subset = None # to be set in the train_balanced.py script
+    
+    model_const = Constants()
+    model_const.relation_classifier = RelationClassifierConstants()
+    model_const.gather_relation = GatherRelationConstants()
+    model_const.gather_relation.hoi_list_json = data_const.hoi_list_json
+    model_const.gather_relation.verb_list_json = data_const.verb_list_json
+
+    train_balanced.main(exp_const,data_const,model_const)
+
+
+def exp_eval():
+    exp_name = 'factors_rcnn_feats_scores_balanced_imgs_per_batch_1'
+    out_base_dir = \
+        '/home/tanmay/Data/weakly_supervised_hoi_exp' + \
+        '/relation_classifier'
+    exp_const = ExpConstants(
+        exp_name=exp_name,
+        out_base_dir=out_base_dir)
+    exp_const.model_dir = os.path.join(exp_const.exp_dir,'models')
+
+    data_const = FeatureBalancedConstants()
+    hoi_cand_dir = '/home/tanmay/Data' + \
+        '/weakly_supervised_hoi_exp/hoi_candidates'
+    data_const.hoi_cands_hdf5 = os.path.join(
+        hoi_cand_dir,
+        'hoi_candidates_test.hdf5')
+    data_const.hoi_cand_labels_hdf5 = os.path.join(
+        hoi_cand_dir,
+        'hoi_candidate_labels_test.hdf5')
+    data_const.faster_rcnn_feats_hdf5 = os.path.join(
+        data_const.proc_dir,
+        'faster_rcnn_fc7.hdf5')
+    data_const.balanced_sampling = False
+    data_const.subset = 'test' # to be set in the train.py script
+    
+    model_const = Constants()
+    model_const.relation_classifier = RelationClassifierConstants()
+    model_const.gather_relation = GatherRelationConstants()
+    model_const.gather_relation.hoi_list_json = data_const.hoi_list_json
+    model_const.gather_relation.verb_list_json = data_const.verb_list_json
+    model_const.relation_classifier.model_num = 145000
+    model_const.relation_classifier.model_pth = os.path.join(
+        exp_const.model_dir,
+        f'relation_classifier_{model_const.relation_classifier.model_num}')
+    evaluate.main(exp_const,data_const,model_const)
 
 
 if __name__=='__main__':
