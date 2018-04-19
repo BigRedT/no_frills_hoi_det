b1d339aed617e03cd7c721198c1cabe2983d8a8e
tgupta6 <tgupta6@illinois.edu>
2018-04-19 14:18:47 -0500

    update paths in compute_map.sh

--- exp/hico_eval/compute_map.sh
+++ exp/hico_eval/compute_map.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SUBSET="test"
-PRED_HOI_DETS_HDF5="/home/tanmay/Data/weakly_supervised_hoi_exp/dets_only/pred_hoi_dets/pred_hoi_dets_${SUBSET}.hdf5"
-OUT_DIR="/home/tanmay/Data/weakly_supervised_hoi_exp/dets_only/mAP_eval/$SUBSET"
+PRED_HOI_DETS_HDF5="/home/tanmay/Data/weakly_supervised_hoi_exp/ho_rcnn/ho_1/pred_hoi_dets.hdf5"
+OUT_DIR="/home/tanmay/Data/weakly_supervised_hoi_exp/ho_rcnn/ho_1/mAP_eval/"
 PROC_DIR="/home/ssd/hico_det_processed_20160224/"
 
 python -m exp.hico_eval.compute_map \
