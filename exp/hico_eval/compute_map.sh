e0e4eb57f9bb6f7e68c3e47172618ce981775d8c
Tanmay Gupta <tanmay2099@gmail.com>
2018-04-21 15:52:21 -0500

    update values in exp/hico_eval/compute_map.sh

--- exp/hico_eval/compute_map.sh
+++ exp/hico_eval/compute_map.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SUBSET="test"
-PRED_HOI_DETS_HDF5="/home/tanmay/Data/weakly_supervised_hoi_exp/ho_rcnn/ho_1/pred_hoi_dets.hdf5"
-OUT_DIR="/home/tanmay/Data/weakly_supervised_hoi_exp/ho_rcnn/ho_1/mAP_eval/"
+PRED_HOI_DETS_HDF5="/home/tanmay/Data/weakly_supervised_hoi_exp/relation_classifier/factors_rcnn_feats_scores_imgs_per_batch_1_focal_loss_False_fp_to_tp_ratio_1000_box_aware_model_True/pred_hoi_dets_${SUBSET}_80000.hdf5"
+OUT_DIR="/home/tanmay/Data/weakly_supervised_hoi_exp/relation_classifier/factors_rcnn_feats_scores_imgs_per_batch_1_focal_loss_False_fp_to_tp_ratio_1000_box_aware_model_True/mAP_eval/${SUBSET}_80000"
 PROC_DIR="/home/ssd/hico_det_processed_20160224/"
 
 python -m exp.hico_eval.compute_map \
