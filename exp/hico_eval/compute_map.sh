25e5acc805c9020f510c1a1ea7b9933f1327a126
Tanmay Gupta <tanmay2099@gmail.com>
2018-04-21 17:06:26 -0500

    update paths in compute_map.sh

--- exp/hico_eval/compute_map.sh
+++ exp/hico_eval/compute_map.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 SUBSET="test"
-PRED_HOI_DETS_HDF5="/home/tanmay/Data/weakly_supervised_hoi_exp/relation_classifier/factors_rcnn_feats_scores_imgs_per_batch_1_focal_loss_False_fp_to_tp_ratio_1000_box_aware_model_True/pred_hoi_dets_${SUBSET}_80000.hdf5"
-OUT_DIR="/home/tanmay/Data/weakly_supervised_hoi_exp/relation_classifier/factors_rcnn_feats_scores_imgs_per_batch_1_focal_loss_False_fp_to_tp_ratio_1000_box_aware_model_True/mAP_eval/${SUBSET}_80000"
-PROC_DIR="/home/ssd/hico_det_processed_20160224/"
+HICO_EXP_DIR="${PWD}/hico_exp"
+PRED_HOI_DETS_HDF5="${HICO_EXP_DIR}/relation_classifier/factors_rcnn_feats_scores_imgs_per_batch_1_focal_loss_False_fp_to_tp_ratio_1000_box_aware_model_True/pred_hoi_dets_${SUBSET}_80000.hdf5"
+OUT_DIR="${HICO_EXP_DIR}/relation_classifier/factors_rcnn_feats_scores_imgs_per_batch_1_focal_loss_False_fp_to_tp_ratio_1000_box_aware_model_True/mAP_eval/${SUBSET}_80000"
+PROC_DIR="${PWD}/hico_processed"
 
 python -m exp.hico_eval.compute_map \
     --pred_hoi_dets_hdf5 $PRED_HOI_DETS_HDF5 \
