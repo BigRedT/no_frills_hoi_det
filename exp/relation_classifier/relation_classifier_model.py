3126579553fbd162994cf34d26056eb77d380057
Tanmay Gupta <tanmay2099@gmail.com>
2018-04-21 15:46:39 -0500

    add BoxAwareRelationClassifierConstants to exp/relation_classifier/relation_classifier_model.py

--- exp/relation_classifier/relation_classifier_model.py
+++ exp/relation_classifier/relation_classifier_model.py
@@ -59,4 +59,35 @@ class RelationClassifier(nn.Module,io.WritableToFile):
         return relation_prob
 
     
+class BoxAwareRelationClassifierConstants(RelationClassifierConstants):
+    def __init__(self):
+        super(BoxAwareRelationClassifierConstants,self).__init__()
+        self.box_feat_size = 12
+
+    @property
+    def box_feature_factor_const(self):
+        factor_const = {
+            'in_dim': self.box_feat_size,
+            'out_dim': self.num_relation_classes,
+            'out_activation': 'Identity',
+            'layer_units': [self.box_feat_size],
+            'activation': 'ReLU',
+            'use_out_bn': False,
+            'use_bn': True
+        }
+        return factor_const
     
+class BoxAwareRelationClassifier(RelationClassifier):
+    def __init__(self,const):
+        super(BoxAwareRelationClassifier,self).__init__(const)
+        self.box_feature_factor = pytorch_layers.create_mlp(
+            self.const.box_feature_factor_const)
+
+    def forward(self,feats):
+        B = feats['human_rcnn'].size(0)
+        faster_rcnn_feature_factor_scores = \
+            self.faster_rcnn_human_feature_factor(feats['human_rcnn']) + \
+            self.faster_rcnn_object_feature_factor(feats['object_rcnn']) + \
+            self.box_feature_factor(feats['box'])
+        relation_prob = self.sigmoid(faster_rcnn_feature_factor_scores)
+        return relation_prob
