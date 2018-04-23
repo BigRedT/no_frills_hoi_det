ec2fb16604664d7a057541eac9ebaa44c28566d6
tgupta6 <tgupta6@illinois.edu>
2018-04-23 12:15:12 -0500

    update exp/hico_eval/compute_map.sh

--- exp/hico_eval/compute_map.sh
+++ exp/hico_eval/compute_map.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 SUBSET="test"
-HICO_EXP_DIR="${PWD}/hico_exp"
-PRED_HOI_DETS_HDF5="${HICO_EXP_DIR}/relation_classifier/factors_rcnn_feats_scores_imgs_per_batch_1_focal_loss_False_fp_to_tp_ratio_1000_box_aware_model_True/pred_hoi_dets_${SUBSET}_80000.hdf5"
-OUT_DIR="${HICO_EXP_DIR}/relation_classifier/factors_rcnn_feats_scores_imgs_per_batch_1_focal_loss_False_fp_to_tp_ratio_1000_box_aware_model_True/mAP_eval/${SUBSET}_80000"
-PROC_DIR="${PWD}/hico_processed"
+HICO_EXP_DIR="${PWD}/data_symlinks/hico_exp"
+EXP_NAME="relation_classifier/factors_rcnn_feats_scores_imgs_per_batch_1_focal_loss_False_fp_to_tp_ratio_1000_box_aware_model_True_box_prob"
+MODEL_NUM="60000"
+PRED_HOI_DETS_HDF5="${HICO_EXP_DIR}/${EXP_NAME}/pred_hoi_dets_${SUBSET}_${MODEL_NUM}.hdf5"
+OUT_DIR="${HICO_EXP_DIR}/${EXP_NAME}/mAP_eval/${SUBSET}_${MODEL_NUM}"
+PROC_DIR="${PWD}/data_symlinks/hico_processed"
 
 python -m exp.hico_eval.compute_map \
     --pred_hoi_dets_hdf5 $PRED_HOI_DETS_HDF5 \
