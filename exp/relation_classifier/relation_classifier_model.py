8edc84051d59d48d5459b63e21009a3078b67e96
tgupta6 <tgupta6@illinois.edu>
2018-04-23 12:21:25 -0500

    update BoxAwareRelationClassifier to have separate prob from box features; works slightly worse than adding the factor score to faster_rcnn_feature_factor_scores (16.6 vs 17.0)

--- exp/relation_classifier/relation_classifier_model.py
+++ exp/relation_classifier/relation_classifier_model.py
@@ -87,7 +87,8 @@ class BoxAwareRelationClassifier(RelationClassifier):
         B = feats['human_rcnn'].size(0)
         faster_rcnn_feature_factor_scores = \
             self.faster_rcnn_human_feature_factor(feats['human_rcnn']) + \
-            self.faster_rcnn_object_feature_factor(feats['object_rcnn']) + \
-            self.box_feature_factor(feats['box'])
-        relation_prob = self.sigmoid(faster_rcnn_feature_factor_scores)
+            self.faster_rcnn_object_feature_factor(feats['object_rcnn']) #+ \
+        box_feature_factor_scores = self.box_feature_factor(feats['box'])
+        relation_prob = self.sigmoid(faster_rcnn_feature_factor_scores) * \
+            self.sigmoid(box_feature_factor_scores)
         return relation_prob
