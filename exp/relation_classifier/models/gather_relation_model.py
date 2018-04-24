64612a7bcbe4cd52a75ed33ef6adaa657525775f
tgupta6 <tgupta6@illinois.edu>
2018-04-24 15:33:21 -0500

    change relation_to_idx variable to relation_to_id in gather_relation_model

--- exp/relation_classifier/models/gather_relation_model.py
+++ exp/relation_classifier/models/gather_relation_model.py
@@ -19,18 +19,18 @@ class GatherRelation(nn.Module,io.WritableToFile):
         super(GatherRelation,self).__init__()
         self.const = copy.deepcopy(const)
         self.hoi_dict = self.get_hoi_dict(self.const.hoi_list_json)
-        self.relation_to_idx = \
-            self.get_relation_to_idx(self.const.verb_list_json)
+        self.relation_to_id = \
+            self.get_relation_to_id(self.const.verb_list_json)
 
     def get_hoi_dict(self,hoi_list_json):
         hoi_list = io.load_json_object(hoi_list_json)
         hoi_dict = {hoi['id']:hoi for hoi in hoi_list}
         return hoi_dict
 
-    def get_relation_to_idx(self,verb_list_json):
+    def get_relation_to_id(self,verb_list_json):
         verb_list = io.load_json_object(verb_list_json)
-        relation_to_idx = {verb['name']: verb['id'] for verb in verb_list}
-        return relation_to_idx
+        relation_to_id = {verb['name']: verb['id'] for verb in verb_list}
+        return relation_to_id
 
     def forward(self,relation_prob):
         batch_size, num_relations = relation_prob.size()
@@ -38,7 +38,7 @@ class GatherRelation(nn.Module,io.WritableToFile):
         output = Variable(torch.zeros(batch_size,num_hois)).cuda()
         for hoi_id, hoi in self.hoi_dict.items():
             relation = hoi['verb']
-            relation_idx = int(self.relation_to_idx[relation])-1
+            relation_idx = int(self.relation_to_id[relation])-1
             hoi_idx = int(hoi_id)-1
             output[:,hoi_idx] = relation_prob[:,relation_idx]
         return output
