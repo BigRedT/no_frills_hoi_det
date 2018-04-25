0ae95ea3807eeb85669d54b6123d10faebeb7b19
tgupta6 <tgupta6@illinois.edu>
2018-04-25 09:59:02 -0500

    functional geometric_factor_model.py

--- exp/relation_classifier/models/geometric_factor_model.py
+++ exp/relation_classifier/models/geometric_factor_model.py
@@ -1,13 +1,23 @@
-class GeometricFactorConstants(RelationClassifierConstants):
+import torch
+import copy
+import torch.nn as nn
+from torch.autograd import Variable
+
+import utils.io as io
+import utils.pytorch_layers as pytorch_layers
+
+
+class GeometricFactorConstants(io.JsonSerializableClass):
     def __init__(self):
         super(GeometricFactorConstants,self).__init__()
-        self.box_feat_size = 12
+        self.box_feat_size = 24
+        self.out_dim = 117
 
     @property
     def box_feature_factor_const(self):
         factor_const = {
             'in_dim': self.box_feat_size,
-            'out_dim': self.num_relation_classes,
+            'out_dim': self.out_dim,
             'out_activation': 'Identity',
             'layer_units': [],
             'activation': 'ReLU',
@@ -17,23 +27,13 @@ class GeometricFactorConstants(RelationClassifierConstants):
         return factor_const
     
     
-class GeometricFactor(RelationClassifier):
+class GeometricFactor(nn.Module,io.WritableToFile):
     def __init__(self,const):
-        super(GeometricFactor,self).__init__(const)
+        super(GeometricFactor,self).__init__()
+        self.const = copy.deepcopy(const)
         self.box_feature_factor = pytorch_layers.create_mlp(
             self.const.box_feature_factor_const)
 
     def forward(self,feats):
-        B = feats['human_rcnn'].size(0)
-        faster_rcnn_feature_factor_scores = \
-            self.faster_rcnn_human_feature_factor(feats['human_rcnn']) + \
-            self.faster_rcnn_object_feature_factor(feats['object_rcnn']) #+ \
         box_feature_factor_scores = self.box_feature_factor(feats['box'])
-        relation_prob = self.sigmoid(faster_rcnn_feature_factor_scores) * \
-            self.sigmoid(box_feature_factor_scores)
-        return relation_prob
-
-    def forward_box_feature_factor(self,feats):
-        box_feature_factor_scores = self.box_feature_factor(feats['box'])
-        box_feature_factor_prob = self.sigmoid(box_feature_factor_scores)
-        return box_feature_factor_prob, box_feature_factor_scores
+        return box_feature_factor_scores
