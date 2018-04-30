a70d661ed0eb44a4c34c8127e541986084167e5a
tgupta6 <tgupta6@illinois.edu>
2018-04-30 12:16:41 -0700

    add outer_product and log quadratic feature transformation in geometric_factor_model

--- exp/relation_classifier/models/geometric_factor_model.py
+++ exp/relation_classifier/models/geometric_factor_model.py
@@ -10,16 +10,22 @@ import utils.pytorch_layers as pytorch_layers
 class GeometricFactorConstants(io.JsonSerializableClass):
     def __init__(self):
         super(GeometricFactorConstants,self).__init__()
-        self.box_feat_size = 24
+        self.box_feat_size = 40 + 40*40 + 80
         self.out_dim = 117
+        self.non_linear = False
 
     @property
     def box_feature_factor_const(self):
+        if self.non_linear:
+            layer_units = [int(self.box_feat_size/2),int(self.box_feat_size/4),int(self.box_feat_size/8)]
+        else:
+            layer_units = []
+
         factor_const = {
             'in_dim': self.box_feat_size,
             'out_dim': self.out_dim,
             'out_activation': 'Identity',
-            'layer_units': [],
+            'layer_units': layer_units,
             'activation': 'ReLU',
             'use_out_bn': False,
             'use_bn': True
@@ -34,8 +40,27 @@ class GeometricFactor(nn.Module,io.WritableToFile):
         self.box_feature_factor = pytorch_layers.create_mlp(
             self.const.box_feature_factor_const)
 
+    def outer_product(self,feat):
+        B,D = feat.size()
+        outer_prod_feat = feat.view(B,1,D) * feat.view(B,D,1)
+        outer_prod_feat = outer_prod_feat.view(B,-1)
+        return outer_prod_feat
+
+    def transform_feat(self,feat):
+        log_feat = torch.log(feat+1e-6)
+        linear_log_feat = torch.cat((feat,log_feat),1)
+        outer_prod_feat = self.outer_product(linear_log_feat)
+        transformed_feat = torch.cat((
+            linear_log_feat,
+            outer_prod_feat),1)
+        return transformed_feat
+
     def forward(self,feats):
-        box_feature_factor_scores = self.box_feature_factor(feats['box'])
+        in_feat = torch.cat((
+            self.transform_feat(feats['box']),
+            feats['object_one_hot']),1)
+        #in_feat = feats['box']
+        box_feature_factor_scores = self.box_feature_factor(in_feat)
         return box_feature_factor_scores
 
 
@@ -47,7 +72,7 @@ class GeometricFactorPairwiseConstants(GeometricFactorConstants):
     def pairwise_linear_const(self):
         const = {
             'in_dim': self.box_feat_size**2,
-            'out_dim': 2000,
+            'out_dim': self.box_feat_size*2,
         }
         return const
 
@@ -65,14 +90,14 @@ class GeometricFactorPairwise(nn.Module,io.WritableToFile):
         super(GeometricFactorPairwise,self).__init__()
         self.const = copy.deepcopy(const)
         
-        self.box_feat_bn = nn.BatchNorm2d(self.const.box_feat_size)
+        #self.box_feat_bn = nn.BatchNorm2d(self.const.box_feat_size)
 
         pairwise_linear_const = self.const.pairwise_linear_const
         self.pairwise_linear = nn.Linear(
             pairwise_linear_const['in_dim'],
             pairwise_linear_const['out_dim'])
         
-        self.pairwise_bn = nn.BatchNorm2d(pairwise_linear_const['out_dim'])
+        #self.pairwise_bn = nn.BatchNorm2d(pairwise_linear_const['out_dim'])
         
         agg_linear_const = self.const.agg_linear_const
         self.agg_linear = nn.Linear(
@@ -93,8 +118,8 @@ class GeometricFactorPairwise(nn.Module,io.WritableToFile):
     def forward(self,feats):
         pairwise_feats = self.get_pairwise_feat(feats['box'])
         pairwise_feats = self.pairwise_linear(pairwise_feats)
-        pairwise_feats = self.pairwise_bn(pairwise_feats)
-        box_feat = self.box_feat_bn(feats['box'])
+        #pairwise_feats = self.pairwise_bn(pairwise_feats)
+        box_feat = feats['box'] #self.box_feat_bn(feats['box'])
         agg_feats = torch.cat((box_feat,pairwise_feats*pairwise_feats),1)
         box_feature_factor_scores = self.agg_linear(agg_feats)
         return box_feature_factor_scores
