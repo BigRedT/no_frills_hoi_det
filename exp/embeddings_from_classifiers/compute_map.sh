f7c639246ee3520aa14e3ec8f005959059ba0190
Tanmay Gupta <tanmay2099@gmail.com>
2018-07-12 12:34:16 -0500

    add compute_map.sh

---
+++ exp/embeddings_from_classifiers/compute_map.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+EXP_SUBNAME=$1
+SUBSET="test"
+HICO_EXP_DIR="${PWD}/data_symlinks/hico_exp"
+EXP_NAME="embeddings_from_classifier/ablation_identity_vs_mlp/${EXP_SUBNAME}" #make_identity_True_adam_weight_decay_1e-6"
+echo $EXP_NAME
+MODEL_NUM="10000"
+PRED_HOI_DETS_HDF5="${HICO_EXP_DIR}/${EXP_NAME}/pred_hoi_dets_${SUBSET}_${MODEL_NUM}.hdf5"
+OUT_DIR="${HICO_EXP_DIR}/${EXP_NAME}/mAP_eval/${SUBSET}_${MODEL_NUM}"
+PROC_DIR="${PWD}/data_symlinks/hico_processed"
+python -m exp.hico_eval.compute_map \
+    --pred_hoi_dets_hdf5 $PRED_HOI_DETS_HDF5 \
+    --out_dir $OUT_DIR \
+    --proc_dir $PROC_DIR \
+    --subset $SUBSET
+
+python -m exp.hico_eval.sample_complexity_analysis \
+    --out_dir $OUT_DIR \
+    --ap_type "AP"
