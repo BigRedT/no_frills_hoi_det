e07e25aaf260bd6f67a81329d588c79cea392e71
Tanmay Gupta <tanmay2099@gmail.com>
2018-07-31 23:54:16 +0530

    update compute_map.sh

--- exp/hico_eval/compute_map.sh
+++ exp/hico_eval/compute_map.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SUBSET="test"
 HICO_EXP_DIR="${PWD}/data_symlinks/hico_exp"
-EXP_NAME="fp_to_tp_ablation/fp_to_tp_ratio_10"
+EXP_NAME="mask_ablation/use_mask_False"
 echo $EXP_NAME
 MODEL_NUM="25000"
 PRED_HOI_DETS_HDF5="${HICO_EXP_DIR}/${EXP_NAME}/pred_hoi_dets_${SUBSET}_${MODEL_NUM}.hdf5"
