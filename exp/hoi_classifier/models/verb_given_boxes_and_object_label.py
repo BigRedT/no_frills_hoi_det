06104729c87cb9cee44d4b8f97a452269ed2736e
Tanmay Gupta <tanmay2099@gmail.com>
2018-08-02 02:18:22 +0530

    add use_object_label and use_log_feat flag in verb_given_boxes...

--- exp/hoi_classifier/models/verb_given_boxes_and_object_label.py
+++ exp/hoi_classifier/models/verb_given_boxes_and_object_label.py
@@ -13,6 +13,8 @@ class VerbGivenBoxesAndObjectLabelConstants(io.JsonSerializableClass):
         self.box_feat_size = 21
         self.num_objects = 80
         self.num_verbs = 117
+        self.use_object_label = True
+        self.use_log_feat = True
 
     @property
     def mlp_const(self):
@@ -37,12 +39,19 @@ class VerbGivenBoxesAndObjectLabel(nn.Module,io.WritableToFile):
         self.mlp = pytorch_layers.create_mlp(self.const.mlp_const)
 
     def transform_feat(self,feat):
-        log_feat = torch.log(torch.abs(feat)+1e-6)
+        if self.const.use_log_feat is True:
+            log_feat = torch.log(torch.abs(feat)+1e-6)
+        else:
+            log_feat = 0*feat
         transformed_feat = torch.cat((feat,log_feat),1) 
         return transformed_feat
 
     def forward(self,feats):
         transformed_box_feats = self.transform_feat(feats['box'])
-        in_feat = torch.cat((transformed_box_feats,feats['object_one_hot']),1)
+        if self.const.use_object_label is True:
+            object_label = feats['object_one_hot']
+        else:
+            object_label = 0*feats['object_one_hot']
+        in_feat = torch.cat((transformed_box_feats,object_label),1)
         factor_scores = self.mlp(in_feat)
         return factor_scores
