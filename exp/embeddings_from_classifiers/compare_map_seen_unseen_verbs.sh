1ad4a9659bc9a1d0d025a8f12f301afb793a721b
Tanmay Gupta <tanmay2099@gmail.com>
2019-04-19 19:27:37 +0530

    delete files

--- exp/embeddings_from_classifiers/compare_map_seen_unseen_verbs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-EXP_SUBNAME=$1
-HICO_EXP_DIR="${PWD}/data_symlinks/hico_exp"
-EXP_NAME="embeddings_from_classifier/aes_and_concept_space/${EXP_SUBNAME}"
-MAP_JSON="${HICO_EXP_DIR}/${EXP_NAME}/mAP_eval_pred_feats/test_35000/mAP.json"
-python -m exp.embeddings_from_classifiers.compare_map_seen_unseen_verbs \
-    --map_json $MAP_JSON
