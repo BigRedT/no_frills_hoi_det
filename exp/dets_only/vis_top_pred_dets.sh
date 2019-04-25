8e4af01e615e0f7192f420cc0f18b29ea7cf8366
Tanmay Gupta <tanmay2099@gmail.com>
2019-04-25 15:48:31 -0500

    path updates

--- exp/dets_only/vis_top_pred_dets.sh
+++ exp/dets_only/vis_top_pred_dets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SUBSET="test"
-PRED_HOI_DETS_HDF5="/home/tanmay/Data/weakly_supervised_hoi_exp/dets_only/pred_hoi_dets/pred_hoi_dets_${SUBSET}.hdf5"
-OUT_DIR="/home/tanmay/Data/weakly_supervised_hoi_exp/dets_only/vis_topk_dets/$SUBSET"
+PRED_HOI_DETS_HDF5="${PWD}/data_symlinks/hico_exp/dets_only/pred_hoi_dets/pred_hoi_dets_${SUBSET}.hdf5"
+OUT_DIR="${PWD}/data_symlinks/hico_exp/dets_only/pred_hoi_dets/vis_topk_dets/$SUBSET"
 
 python -m exp.dets_only.vis_top_pred_dets \
     --pred_hoi_dets_hdf5 $PRED_HOI_DETS_HDF5 \
