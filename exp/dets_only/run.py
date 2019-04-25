fd4dca789639e2bf37b1dafc743cf73d3a32e96e
Tanmay Gupta <tanmay2099@gmail.com>
2019-04-25 15:44:21 -0500

    simplify dets only

--- exp/dets_only/run.py
+++ exp/dets_only/run.py
@@ -8,18 +8,14 @@ from utils.constants import ExpConstants
 
 def exp_detect_hoi():
     exp_name = 'dets_only'
-    exp_const = ExpConstants(
-        exp_name=exp_name,
-        out_base_dir='/home/tanmay/Data/weakly_supervised_hoi_exp')
+    exp_const = ExpConstants(exp_name=exp_name)
     exp_const.subset = 'test'
 
-    data_const = HicoConstants(
-        clean_dir='/home/ssd/hico_det_clean_20160224',
-        proc_dir='/home/ssd/hico_det_processed_20160224')
-    data_const.selected_dets_hdf5 = '/home/tanmay/Data/' + \
-        'weakly_supervised_hoi_exp/select_confident_boxes_in_hico/' + \
-        'select_boxes_human_thresh_0.01_max_10_object_thresh_0.01_max_10/' + \
-        'selected_coco_cls_dets.hdf5'
+    data_const = HicoConstants()
+    data_const.selected_dets_hdf5 = os.path.join(
+        os.getcwd(),
+        'data_symlinks/hico_exp/' + \
+        'select_confident_boxes_in_hico/selected_coco_cls_dets.hdf5')
     
     predict_hois.main(exp_const,data_const)
 
