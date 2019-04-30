b1687f2068452ccca59c2c8628e181176fa9a032
Tanmay Gupta <tanmay2099@gmail.com>
2019-04-30 12:21:46 -0500

    delete dets_only

--- exp/dets_only/vis_top_pred_dets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SUBSET="test"
-PRED_HOI_DETS_HDF5="${PWD}/data_symlinks/hico_exp/dets_only/pred_hoi_dets/pred_hoi_dets_${SUBSET}.hdf5"
-OUT_DIR="${PWD}/data_symlinks/hico_exp/dets_only/pred_hoi_dets/vis_topk_dets/$SUBSET"
-
-python -m exp.dets_only.vis_top_pred_dets \
-    --pred_hoi_dets_hdf5 $PRED_HOI_DETS_HDF5 \
-    --out_dir $OUT_DIR \
-    --subset $SUBSET
