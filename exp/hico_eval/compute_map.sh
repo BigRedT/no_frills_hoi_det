2352961aa0a6458a1ce4426ed4a66677c173c26c
Tanmay Gupta <tanmay2099@gmail.com>
2019-05-01 18:30:48 -0500

    update eval

--- exp/hico_eval/compute_map.sh
+++ exp/hico_eval/compute_map.sh
@@ -7,8 +7,13 @@ MODEL_NUM="30000"
 PRED_HOI_DETS_HDF5="${HICO_EXP_DIR}/${EXP_NAME}/pred_hoi_dets_${SUBSET}_${MODEL_NUM}.hdf5"
 OUT_DIR="${HICO_EXP_DIR}/${EXP_NAME}/mAP_eval/${SUBSET}_${MODEL_NUM}"
 PROC_DIR="${PWD}/data_symlinks/hico_processed"
+
 python -m exp.hico_eval.compute_map \
     --pred_hoi_dets_hdf5 $PRED_HOI_DETS_HDF5 \
     --out_dir $OUT_DIR \
     --proc_dir $PROC_DIR \
     --subset $SUBSET
+
+python -m exp.hico_eval.sample_complexity_analysis \
+    --out_dir $OUT_DIR \
+    --ap_type "AP"
